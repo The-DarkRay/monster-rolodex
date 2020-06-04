30ada874ee45341c9732aca9bf51901f4de71a11
The-DarkRay <doraramkmuarrockzz@gmail.com>
2020-06-04 10:29:53 +0530

    Updates

---
+++ precache-manifest.916ca285ba889907ba4565b0e33e1ed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59452c10c4a3c27f41348de3541936d",
+    "url": "/https//The-DarkRay.github.io/monster.rolodex/index.html"
+  },
+  {
+    "revision": "c9a4419f15402bd43ab3",
+    "url": "/https//The-DarkRay.github.io/monster.rolodex/static/css/main.16c90b04.chunk.css"
+  },
+  {
+    "revision": "758750277b3ce6fb47ec",
+    "url": "/https//The-DarkRay.github.io/monster.rolodex/static/js/2.03915027.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/https//The-DarkRay.github.io/monster.rolodex/static/js/2.03915027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9a4419f15402bd43ab3",
+    "url": "/https//The-DarkRay.github.io/monster.rolodex/static/js/main.929b3267.chunk.js"
+  },
+  {
+    "revision": "73731e425ef5f68e130d",
+    "url": "/https//The-DarkRay.github.io/monster.rolodex/static/js/runtime-main.634ce2a6.js"
+  }
+]);
